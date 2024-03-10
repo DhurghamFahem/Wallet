a977ecc081624295bf2606a543951393ef21c827
Dhurgham <drm.fhm@gmail.com>
2024-03-10 17:05:11 +0300

    finish the card

--- src/home/components/homeCard.jsx
+++ src/home/components/homeCard.jsx
@@ -1,9 +1,16 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import Delete from "../../../assets/svgs/delete.svg";
+import Edit from "../../../assets/svgs/edit.svg";
 
 const HomeCard = ({ type, onPress }) => {
   const bgColor = type === "income" ? "#7FB7BE" : "#7D1538";
   const currentStyles = styles(bgColor);
+
+  const getAmount = () => {
+    return type === "income" ? "+" + "5,000" : "-" + "5,000";
+  };
+
   return (
     <View style={currentStyles.container}>
       <View style={currentStyles.bar}></View>
@@ -29,10 +36,50 @@ const HomeCard = ({ type, onPress }) => {
               justifyContent: "center",
             }}
           >
-            <Text style={currentStyles.amountText}>5000</Text>
+            <Text style={currentStyles.amountText}>{getAmount()}</Text>
             <Text style={currentStyles.dateText}>01/02/2023</Text>
             <Text style={currentStyles.dateText}>06:30 AM</Text>
           </View>
+        </View>
+
+        <View
+          style={{
+            height: 50,
+            backgroundColor: "red",
+            width: "96%",
+            flexDirection: "row",
+            borderRadius: 20,
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#7D1538",
+              width: "50%",
+              height: 50,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Delete height={30} width={30} fill={"#DACC3E"} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#7FB7BE",
+              width: "50%",
+              height: 50,
+              borderTopRightRadius: 20,
+              borderBottomRightRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Edit height={30} width={30} fill={"#DACC3E"} />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -42,7 +89,7 @@ const HomeCard = ({ type, onPress }) => {
 const styles = (bgColor) =>
   StyleSheet.create({
     container: {
-      height: 100,
+      height: 150,
       width: "96%",
       backgroundColor: "#DACC3E",
       borderTopRightRadius: 20,
@@ -58,7 +105,7 @@ const styles = (bgColor) =>
     bar: {
       backgroundColor: bgColor,
       width: 10,
-      height: 100,
+      height: 150,
       borderTopLeftRadius: 20,
       borderBottomLeftRadius: 20,
     },
